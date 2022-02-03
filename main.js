9bf7614d6f9a45c731ea0e992ec93f72f7d0552e
Ashish <bleshbanz@gmail.com>
2022-02-03 18:58:12 +0530

    index.html file updated made a <ul><li> listing

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+var list = {
+  'Intro Vue' : 'intro.html',
+  'Vue Instance' : 'vue_instance.html',
+  'Vue Life cycle hook' : 'lifecycle_hooks.html',
+}
+var string  = '';
+
+for (const property in list) {
+  // console.log(`${property}: ${list[property]}`);
+  string = string + `<li><a href="`+list[property]+`" target="_blank">`+property+`</a></li>\n`;
+}
+console.log(string);
+document.querySelector('#list').innerHTML = `
+  <ul>
+    `+string+`
+  </ul>
+`
